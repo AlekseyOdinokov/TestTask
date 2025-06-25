6859740df740371a312c639138fd27faa3d5a03e
AlekseyOdinokov <165428607+AlekseyOdinokov@users.noreply.github.com>
2025-06-26 00:06:54 +0300

    Add files via upload

---
+++ 1Task/Interfaces/IAuthenticationService.cs
@@ -0,0 +1,15 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface IAuthenticationService
+    {
+        bool Register(string username, string password, string role);
+        bool Login(string username, string password, out User user);
+    }
+}
---
+++ 1Task/Interfaces/ITaskService.cs
@@ -0,0 +1,16 @@
+﻿using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    public interface ITaskService
+    {
+        void CreateTask(string title, string description, string assignee);
+        List<TaskItem> GetTasksForUser(string username);
+        void ChangeStatus(int taskId, Models.TaskStatus newStatus);
+    }
+}
